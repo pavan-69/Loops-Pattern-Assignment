121e3daf2230eb53f40eb1d7edb74d0dd8ade002
pavan <pavabbubg@gmail.com>
2023-03-02 14:18:32 +0530

    first commit

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+public class Triangle {
+    public static void main(String[] args) {
+        int i, j;
+        int n = 11;
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (i == (n - 1) / 2 || i + j >= (n - 1) / 2 && i <= (n - 1) / 2 && j - i <= (n - 1) / 2) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
